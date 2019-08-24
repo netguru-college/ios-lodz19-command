119f9a2b69fd2c5920afa413bde39b51f2d75dca
Michał Warchał <michal.warchal@netguru.co>
2019-08-24 15:23:29 +0200

    Fix issue with collection view cell

--- Project/Source Files/Screens/Meal/MealCollectionViewCell.swift
+++ Project/Source Files/Screens/Meal/MealCollectionViewCell.swift
@@ -3,34 +3,32 @@
 //  NetguruCollegeApp
 //
 
-
 import UIKit
 
-class MealCollectionViewCell: UICollectionViewCell {
+final class MealCollectionViewCell: UICollectionViewCell {
+
+    static let name =  "MealCollectionViewCell"
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .white
         addSubview(mealImageView)
         NSLayoutConstraint.activate([
-            mealImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            mealImageView.topAnchor.constraint(equalTo: topAnchor),
             mealImageView.leftAnchor.constraint(equalTo: leftAnchor),
             mealImageView.rightAnchor.constraint(equalTo: rightAnchor),
-            mealImageView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            mealImageView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     let mealImageView: UIImageView = {
-        var mealImageView = UIImageView()
-        mealImageView.translatesAutoresizingMaskIntoConstraints = true
+        let mealImageView = UIImageView()
+        mealImageView.contentMode = .scaleAspectFit
+        mealImageView.translatesAutoresizingMaskIntoConstraints = false
         return mealImageView
     }()
-
-}
-extension MealCollectionViewCell {
-    static let name: String =  "MealCollectionViewCell"
 }
