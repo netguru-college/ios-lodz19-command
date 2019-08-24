9074201e68e73f67685af8104d12926bd58e09f6
Jan Malewski <malewski.js@gmail.com>
2019-08-24 13:55:06 +0200

    Add cuisine service

---
+++ Project/Source Files/Services/CuisineGenerator.swift
@@ -0,0 +1,42 @@
+//
+//  CuisineGenerator.swift
+//  NetguruCollegeApp
+//
+
+import Foundation
+
+class CuisineGenerator {
+
+    private let cuisineList: [String] = [
+        "African",
+        "American",
+        "British",
+        "Cajun",
+        "Caribbean",
+        "Chinese",
+        "Eastern European",
+        "European",
+        "French",
+        "German",
+        "Greek",
+        "Indian",
+        "Irish",
+        "Italian",
+        "Japanese",
+        "Jewish",
+        "Korean",
+        "Latin American",
+        "Mediterranean",
+        "Mexican",
+        "Middle Eastern",
+        "Nordic",
+        "Southern",
+        "Spanish",
+        "Thai",
+        "Vietnamese"]
+
+    func getRandom() -> String? {
+        return cuisineList.randomElement()
+    }
+
+}
